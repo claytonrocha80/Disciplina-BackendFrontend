e180a50eae74715d89f6a6859a8e1d24d69faa20
claytonrocha80 <clayton.rocha@coopersystem.com.br>
2026-04-07 18:48:46 -0300

    funcionalidades node js

---
+++ hello-npm/index.js
@@ -0,0 +1,19 @@
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'nome',
+      message: 'Qual é o seu nome?',
+    },
+    {
+      type: 'rawlist',
+      name: 'idade',
+      message: 'Qual é a sua idade?',
+      choices: ['29 a 39', '40 a 50', '50+'],
+    },
+  ])
+  .then((answers) => {
+    console.log('Oi ' + answers.nome + ', você tem ' + answers.idade + ' anos');
+  });
