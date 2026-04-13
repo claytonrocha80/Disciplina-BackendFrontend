10e92d6c40ebc207a86837b696eee9b9ef04b537
claytonrocha80 <clayton.rocha@coopersystem.com.br>
2026-04-13 08:57:19 -0300

    tentativa front

--- HERO CLOUD - PROJETO/lgpd-front/src/app/app.module.ts
+++ HERO CLOUD - PROJETO/lgpd-front/src/app/app.module.ts
@@ -1,9 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {SharedModule} from "./shared/shared.module";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {SharedModule} from "./shared/shared.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
